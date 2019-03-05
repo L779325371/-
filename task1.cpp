3ef23b13ae778744adcdcfcf167d54deb7e4c41f
L779325371 <47714511+L779325371@users.noreply.github.com>
2019-03-05 21:32:59 +0800

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc,char ** argv){
+
+	printf("hello world!\n");
+	
+	return 0;
+	/*
+	
+	argc是参数个数..argv是参数字符数组或者字符数组指针... 以前都是char * argv[]  
+	
+	这个写的char ** argv让我理解了多一维数组就多一层指针
+	
+	
+	
+	*/
+
+}
